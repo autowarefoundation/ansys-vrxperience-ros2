4b0a4cafaf173184093d57867a169b48553cf432
Joshua Whitley <josh.whitley@autoware.org>
2020-10-15 18:24:56 -0500

    Updating senders and receivers to be Components.
    Signed-off-by: Joshua Whitley <josh.whitley@autoware.org>

--- vrxperience_bridge/src/recv_laser_meter.cpp
+++ vrxperience_bridge/src/recv_laser_meter.cpp
@@ -12,47 +12,63 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <string>
+
 #include "vrxperience_bridge/sim_data_receiver.hpp"
 #include "vrxperience_msgs/msg/laser_meter.hpp"
 #include "IndyDS_LaserMeter.h"
 
-using vrxperience_bridge::SimDataReceiver;
-typedef SimDataReceiver<IndyDS_LaserMeter, vrxperience_msgs::msg::LaserMeter> LaserMeterReceiver;
-
-std::string sensor_frame;  // NOLINT
-
-void convert(IndyDS_LaserMeter IN simMsg, vrxperience_msgs::msg::LaserMeter OUT rosMsg)
+namespace vrxperience_bridge
 {
-  rosMsg.header.stamp.sec = static_cast<int>(simMsg.lastUpdate);
-  rosMsg.header.stamp.nanosec =
-    (static_cast<int>(simMsg.lastUpdate * 1e9)) % (static_cast<int>(1e9));
-  rosMsg.header.frame_id = sensor_frame;
 
-  rosMsg.global_id = simMsg.globalId;
-  rosMsg.ego_vehicle_id = simMsg.vhlId;
-  rosMsg.sensor_id = simMsg.sensorId;
-  rosMsg.nearest_point = simMsg.nearestPoint;
-
-  for (uint32_t i = 0; i < simMsg.resultArray._length; i++) {
-    auto simLaserMeterPointMsg = simMsg.resultArray._buffer[i];
-    vrxperience_msgs::msg::LaserMeterPoint rosLaserMeterPointMsg;
-
-    rosLaserMeterPointMsg.hit = simLaserMeterPointMsg.hit;
-    rosLaserMeterPointMsg.h_angle = simLaserMeterPointMsg.Hangle;
-    rosLaserMeterPointMsg.v_angle = simLaserMeterPointMsg.Vangle;
-    rosLaserMeterPointMsg.intersection_point.x = simLaserMeterPointMsg.relposx;
-    rosLaserMeterPointMsg.intersection_point.y = simLaserMeterPointMsg.relposy;
-    rosLaserMeterPointMsg.intersection_point.z = simLaserMeterPointMsg.relposz;
-    rosLaserMeterPointMsg.distance = simLaserMeterPointMsg.distance;
-
-    rosMsg.results.push_back(rosLaserMeterPointMsg);
+class LaserMeterReceiver
+  : public SimDataReceiver<IndyDS_LaserMeter, vrxperience_msgs::msg::LaserMeter>
+{
+public:
+  explicit LaserMeterReceiver(const rclcpp::NodeOptions & options)
+  : SimDataReceiver(
+      "recv_movable_targets",
+      options,
+      IndyDS_LaserMeter_desc,
+      std::bind(&LaserMeterReceiver::convert, this, _1, _2)
+  )
+  {
+    sensor_frame_ = declare_parameter("sensor_frame", "");
   }
-}
 
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  LaserMeterReceiver receiver("recv_movable_targets", IndyDS_LaserMeter_desc, &convert);
-  sensor_frame = receiver.declare_parameter("sensor_frame", "");
-  receiver.run();
-}
+  void convert(IndyDS_LaserMeter IN simMsg, vrxperience_msgs::msg::LaserMeter OUT rosMsg)
+  {
+    rosMsg.header.stamp.sec = static_cast<int>(simMsg.lastUpdate);
+    rosMsg.header.stamp.nanosec =
+      (static_cast<int>(simMsg.lastUpdate * 1e9)) % (static_cast<int>(1e9));
+    rosMsg.header.frame_id = sensor_frame_;
+
+    rosMsg.global_id = simMsg.globalId;
+    rosMsg.ego_vehicle_id = simMsg.vhlId;
+    rosMsg.sensor_id = simMsg.sensorId;
+    rosMsg.nearest_point = simMsg.nearestPoint;
+
+    for (uint32_t i = 0; i < simMsg.resultArray._length; i++) {
+      auto simLaserMeterPointMsg = simMsg.resultArray._buffer[i];
+      vrxperience_msgs::msg::LaserMeterPoint rosLaserMeterPointMsg;
+
+      rosLaserMeterPointMsg.hit = simLaserMeterPointMsg.hit;
+      rosLaserMeterPointMsg.h_angle = simLaserMeterPointMsg.Hangle;
+      rosLaserMeterPointMsg.v_angle = simLaserMeterPointMsg.Vangle;
+      rosLaserMeterPointMsg.intersection_point.x = simLaserMeterPointMsg.relposx;
+      rosLaserMeterPointMsg.intersection_point.y = simLaserMeterPointMsg.relposy;
+      rosLaserMeterPointMsg.intersection_point.z = simLaserMeterPointMsg.relposz;
+      rosLaserMeterPointMsg.distance = simLaserMeterPointMsg.distance;
+
+      rosMsg.results.push_back(rosLaserMeterPointMsg);
+    }
+  }
+
+private:
+  std::string sensor_frame_;
+};  // class LaserMeterReceiver
+
+}  // namespace vrxperience_bridge
+
+#include <rclcpp_components/register_node_macro.hpp>  // NOLINT
+RCLCPP_COMPONENTS_REGISTER_NODE(vrxperience_bridge::LaserMeterReceiver)
--- vrxperience_bridge/src/send_cab_to_model_corrective.cpp
+++ vrxperience_bridge/src/send_cab_to_model_corrective.cpp
@@ -12,46 +12,53 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <memory>
-
 #include "vrxperience_bridge/sim_data_sender.hpp"
 #include "vrxperience_msgs/msg/cab_to_model_corrective.hpp"
 #include "IndyDS_CabToModelCorrective.h"
 
-using vrxperience_bridge::SimDataSender;
-typedef SimDataSender<vrxperience_msgs::msg::CabToModelCorrective,
-    IndyDS_CabToModelCorrective> CabToModelCorrectiveSender;
-
-void convert(
-  vrxperience_msgs::msg::CabToModelCorrective IN rosMsg,
-  IndyDS_CabToModelCorrective OUT simMsg)
+namespace vrxperience_bridge
 {
-  simMsg.TimeOfUpdate = rosMsg.header.stamp.sec + rosMsg.header.stamp.nanosec * 1e-9;
-  simMsg.AcceleratorAdditive = rosMsg.accelerator_additive;
-  simMsg.AcceleratorMultiplicative = rosMsg.accelerator_multiplicative;
-  simMsg.BrakeAdditive = rosMsg.brake_additive;
-  simMsg.BrakeMultiplicative = rosMsg.brake_multiplicative;
-  simMsg.ClutchAdditive = rosMsg.clutch_additive;
-  simMsg.ClutchMultiplicative = rosMsg.clutch_multiplicative;
-  simMsg.ParkingBrakeAdditive = rosMsg.parking_brake_additive;
-  simMsg.ParkingBrakeMultiplicative = rosMsg.parking_brake_multiplicative;
-  simMsg.GearboxTakeOver = rosMsg.gear_box_take_over;
-  simMsg.GearboxAutoMode = rosMsg.gear_box_auto_mode;
-  simMsg.WantedGear = rosMsg.wanted_gear;
-  simMsg.ShiftUp = rosMsg.shift_up;
-  simMsg.ShiftDown = rosMsg.shift_down;
-  simMsg.IsRatioLimit = rosMsg.is_ratio_limit;
-  simMsg.MinRatio = rosMsg.min_ratio;
-  simMsg.MaxRatio = rosMsg.max_ratio;
-}
 
-int main(int argc, char * argv[])
+class CabToModelCorrectiveSender
+  : public SimDataSender<vrxperience_msgs::msg::CabToModelCorrective,
+    IndyDS_CabToModelCorrective>
 {
-  rclcpp::init(argc, argv);
-  auto sender = std::make_shared<CabToModelCorrectiveSender>(
-    "send_cab_to_model_corrective",
-    IndyDS_CabToModelCorrective_desc,
-    &convert);
+public:
+  explicit CabToModelCorrectiveSender(const rclcpp::NodeOptions & options)
+  : SimDataSender(
+      "send_cab_to_model_corrective",
+      options,
+      IndyDS_CabToModelCorrective_desc,
+      std::bind(&CabToModelCorrectiveSender::convert, this, _1, _2)
+  )
+  {
+  }
 
-  rclcpp::spin(sender);
-}
+  void convert(
+    vrxperience_msgs::msg::CabToModelCorrective IN rosMsg,
+    IndyDS_CabToModelCorrective OUT simMsg)
+  {
+    simMsg.TimeOfUpdate = rosMsg.header.stamp.sec + rosMsg.header.stamp.nanosec * 1e-9;
+    simMsg.AcceleratorAdditive = rosMsg.accelerator_additive;
+    simMsg.AcceleratorMultiplicative = rosMsg.accelerator_multiplicative;
+    simMsg.BrakeAdditive = rosMsg.brake_additive;
+    simMsg.BrakeMultiplicative = rosMsg.brake_multiplicative;
+    simMsg.ClutchAdditive = rosMsg.clutch_additive;
+    simMsg.ClutchMultiplicative = rosMsg.clutch_multiplicative;
+    simMsg.ParkingBrakeAdditive = rosMsg.parking_brake_additive;
+    simMsg.ParkingBrakeMultiplicative = rosMsg.parking_brake_multiplicative;
+    simMsg.GearboxTakeOver = rosMsg.gear_box_take_over;
+    simMsg.GearboxAutoMode = rosMsg.gear_box_auto_mode;
+    simMsg.WantedGear = rosMsg.wanted_gear;
+    simMsg.ShiftUp = rosMsg.shift_up;
+    simMsg.ShiftDown = rosMsg.shift_down;
+    simMsg.IsRatioLimit = rosMsg.is_ratio_limit;
+    simMsg.MinRatio = rosMsg.min_ratio;
+    simMsg.MaxRatio = rosMsg.max_ratio;
+  }
+};  // class CabToModelCorrectiveSender
+
+}  // namespace vrxperience_bridge
+
+#include <rclcpp_components/register_node_macro.hpp>  // NOLINT
+RCLCPP_COMPONENTS_REGISTER_NODE(vrxperience_bridge::CabToModelCorrectiveSender)
